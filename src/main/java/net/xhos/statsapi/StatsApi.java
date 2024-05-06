6d5463ca6525b59d0609b943594c5b265e028d79
Mark Kudriavtsev <mkudriavtsev@myseneca.ca>
2024-05-06 13:54:48 -0400

    some initial changes

--- src/main/java/net/xhos/statsapi/StatsApi.java
+++ src/main/java/net/xhos/statsapi/StatsApi.java
@@ -5,11 +5,12 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Statsapi implements ModInitializer {
+public class StatsApi implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("stats-api");
+	public static final String MOD_ID = "statsapi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/xhos/statsapi/StatsApiDataGenerator.java
+++ src/main/java/net/xhos/statsapi/StatsApiDataGenerator.java
@@ -3,7 +3,7 @@ package net.xhos.statsapi;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class StatsapiDataGenerator implements DataGeneratorEntrypoint {
+public class StatsApiDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
