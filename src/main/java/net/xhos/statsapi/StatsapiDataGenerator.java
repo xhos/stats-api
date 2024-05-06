8caec8a813d39d77cdb5628052e8ee5488727b03
Mark Kudriavtsev <mkudriavtsev@myseneca.ca>
2024-05-05 23:01:55 -0400

    initial commit

---
+++ src/main/java/net/xhos/statsapi/StatsapiDataGenerator.java
@@ -0,0 +1,11 @@
+package net.xhos.statsapi;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StatsapiDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
